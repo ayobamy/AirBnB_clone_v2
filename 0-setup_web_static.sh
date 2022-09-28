5bdbdd26f57611e4776db635218d2465f58b1e55
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-28 19:34:30 +0000

    [feat]: a script to setup my web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# a script to setup my web servers for the deployment of web_static
+
+# update packages and install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
