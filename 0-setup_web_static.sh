c2f33c5f51ec911484a784162033b323d85e4649
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-09-29 19:10:59 +0000

    [feat]: setting up my server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,3 +5,30 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
+# creating paths
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+
+# Print Welcome message
+echo "Welcome to my site!" >> /data/web_static/releases/test/index.html
+
+# Check if directory current exist
+if [ -d "/data/web_static/current" ]
+then
+        sudo rm -rf /data/web_static/current
+fi
+
+# creating a symbolink link  linked to the folder
+ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the file content
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restart nginx server
+sudo service nginx restart
+
+# exit with status
+exit 0
