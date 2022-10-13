e3d7584a39f5500efe991d195053c74362875fde
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-13 21:01:30 +0000

    [feat]: Flask - displays Python text route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,12 +25,13 @@ def hbnb_route():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
-	"""displays value of /c/<text> web app route"""
+	"""displays value of /c/<string:text> web app route"""
 	text = text.replace("_", " ")
 	return f"C {text}"
 
-@app.route("/python/(<text>)", strict_slashes=False)
-def c_route(text="is cool"):
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def python_route(text="is cool"):
 	"""displays value of /python/(<text>) web app route"""
 	text = text.replace("_", " ")
 	return f"Python {text}"
