109d4ed9a80e67be9ca16fd468a321e7b9bef363
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-13 20:13:58 +0000

    [feat]: Flask - displays /c/<text> route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application:
+listens on 0.0.0.0, port 5000
+Routes:	/: display “Hello HBNB!”
+		/hbnb: display “HBNB”
+		/c/<text>: display “C” followed by the value of text
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+	"""function for web app home route"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+	"""displays /hbnb web app route"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route():
+	"""displays /c/<text> web app route"""
+	return 
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
