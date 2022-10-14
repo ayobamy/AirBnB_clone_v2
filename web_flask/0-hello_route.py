c45484c72e0d09cf8f5e18d75ac148d97e06ad32
ayobamy <ayobamyahmed@gmail.com>
2022-10-14 08:21:16 -0700

    [fix]: fixed pycodestyle error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_route():
-	"""function for web app route"""
-	return "Hello HBNB!"
+    """function for web app route"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
