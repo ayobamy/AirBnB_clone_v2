d7a834c274113c665257f5eb900c2573244ecc69
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-14 18:47:01 +0000

    [fix]: fixed bug

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -41,7 +41,6 @@ def python_route(text="is cool"):
     return "Python %s" % text
 
 
-@app.route('/number', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     """displays /number/<n> web app route"""
