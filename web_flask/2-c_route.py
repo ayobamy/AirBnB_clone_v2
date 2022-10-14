cd7f0f283240ca741e3ebee1d7cd7197d896ebb4
ayobamy <ayobamyahmed@gmail.com>
2022-10-14 08:28:42 -0700

    [fix]: fixed pycodestyle error

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,30 +3,32 @@
 a script that starts a Flask web application:
 listens on 0.0.0.0, port 5000
 Routes:	/: display “Hello HBNB!”
-		/hbnb: display “HBNB”
-		/c/<text>: display “C” followed by the value of text
+    /hbnb: display “HBNB”
+    /c/<text>: display “C” followed by the value of text
 """
-
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_route():
-	"""function for web app home route"""
-	return "Hello HBNB!"
+    """function for web app home route"""
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_route():
-	"""displays /hbnb web app route"""
-	return "HBNB"
+    """displays /hbnb web app route"""
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
-	"""displays value of /c/<text> web app route"""
-	text = text.replace("_", " ")
-	return f"C {text}"
+    """displays value of /c/<text> web app route"""
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 
 if __name__ == "__main__":
