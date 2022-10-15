9fc47e16740b70478de64d504d92848dde7e1e63
ayobamy <ayobamyahmed@gmail.com>
2022-10-15 11:54:02 -0700

    [update]: updated state class

--- models/state.py
+++ models/state.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+
+class State(BaseModel, Base):
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
 class State(BaseModel):
     """ State class """
