3d3175278b1ba65baa356a2b9d9886faac7ac429
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-16 19:13:05 +0000

    [update]: updated user class

--- models/user.py
+++ models/user.py
@@ -3,12 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
